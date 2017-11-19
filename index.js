de6b1756e42cbcb16445dd95e1258ae4b85dd2b8
kent winter <kgw700@gmail.com>
2017-11-19 03:21:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function Beatles Loops () {
-  function theBeatlesPlay (musicians, instruments){
-    var empty = {};
+function theBeatlesPlay (musicians, instruments){
+    var empty = [];
     for (let i = 0; i < musicians.length; i++){
-        empty.push("musicians[i]" + "plays" + "instruments[i]")
+        empty.push(musicians[i] + "plays" + instruments[i])
       }
       return empty;
-  }
-  function johnLennonFacts (facts){
-    i=0;
+}
+
+function johnLennonFacts(facts){
+    let i=0;
+    var newfacts = [];
     while (i<facts.length) {
-      facts[i]="facts[i]"+"!!!";
-      ++i;
+      newfacts.push(facts[i] + "!!!", ...facts);
+      i++;
     }
     return facts;
-  }
 }
